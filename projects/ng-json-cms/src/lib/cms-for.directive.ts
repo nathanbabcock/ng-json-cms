f6c57f5efb4dc843dc556662a4f3f8e23c61e997
Nathan Babcock <nathan.babcock@motorolasolutions.com>
2019-02-08 15:37:33 +0100

    CmsFor pulls from json

--- projects/ng-json-cms/src/lib/cms-for.directive.ts
+++ projects/ng-json-cms/src/lib/cms-for.directive.ts
@@ -6,15 +6,20 @@ import { ContentService } from './content-service.service';
 })
 export class CmsForOf implements OnInit, OnChanges {
   @Input() cmsFor: any;
-  @Input() source: string;
+  // @Input() source: string;
 
   constructor(private container: ViewContainerRef, private template: TemplateRef<any>, private contentService: ContentService) { }
 
   ngOnChanges() {
+    console.log(this.cmsFor);
     this.container.clear();
-    for (const input of [1, 2]) {
-      this.container.createEmbeddedView(this.template);
-    }
+    this.contentService.query(this.cmsFor).then(content => {
+      console.log(content);
+      for (const input of content) {
+        console.log(input);
+        this.container.createEmbeddedView(this.template);
+      }
+    });
   }
 
   ngOnInit(): void {
---
+++ projects/ng-json-cms/src/lib/cms-for/cms-for.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[cms-for]',
+  templateUrl: './cms-for.component.html',
+  styleUrls: ['./cms-for.component.css']
+})
+export class CmsForComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
