521e36748877bb09b7568eebd24bd620a8706a0f
Nathan Babcock <nathan.babcock@motorolasolutions.com>
2019-02-08 09:39:25 +0100

    import lib into demo

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgJsonCmsModule } from 'projects/ng-json-cms/src/public_api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgJsonCmsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
